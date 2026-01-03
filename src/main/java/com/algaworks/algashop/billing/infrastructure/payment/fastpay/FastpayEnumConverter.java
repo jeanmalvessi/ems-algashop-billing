419d7b70403d7879e8aa26c435ebd00c712e7470
Jean Malvessi <jeanmalvessi@gmail.com>
2026-01-03 11:05:00 -0300

    Fastpay API integration

---
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/payment/fastpay/FastpayEnumConverter.java
@@ -0,0 +1,27 @@
+package com.algaworks.algashop.billing.infrastructure.payment.fastpay;
+
+import com.algaworks.algashop.billing.domain.model.invoice.PaymentMethod;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentStatus;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class FastpayEnumConverter {
+
+    public static PaymentMethod convert(FastpayPaymentMethod method) {
+        return switch (method) {
+            case CREDIT -> PaymentMethod.CREDIT_CARD;
+            case GATEWAY_BALANCE -> PaymentMethod.GATEWAY_BALANCE;
+        };
+    }
+
+    public static PaymentStatus convert(FastpayPaymentStatus status) {
+        return switch (status) {
+            case PENDING -> PaymentStatus.PENDING;
+            case PROCESSING -> PaymentStatus.PROCESSING;
+            case FAILED, CANCELED -> PaymentStatus.FAILED;
+            case PAID -> PaymentStatus.PAID;
+            case REFUNDED -> PaymentStatus.REFUNDED;
+        };
+    }
+}
